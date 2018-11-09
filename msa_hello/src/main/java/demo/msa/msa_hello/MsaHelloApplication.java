a73619513b8585b68df86438ef0c1a1c1d4fdff1
stephenxe <317166932@qq.com>
2018-11-09 22:32:41 +0800

    title: 初始化工程01 name: xukaifang

---
+++ msa_hello/src/main/java/demo/msa/msa_hello/MsaHelloApplication.java
@@ -0,0 +1,12 @@
+package demo.msa.msa_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaHelloApplication.class, args);
+	}
+}
